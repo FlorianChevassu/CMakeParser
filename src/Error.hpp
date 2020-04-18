4eb22df486b0b3fdba2facc20221e0f1bab45050
Xaxetrov <edern.haumont@gmail.com>
2020-04-18 11:42:26 +0200

    Add Error fluent builder

--- src/Error.hpp
+++ src/Error.hpp
@@ -6,10 +6,26 @@
 
 namespace cmake::language
 {
+  struct ErrorBuilder;
+
   struct Error
   {
+    static ErrorBuilder make();
+
     Range context;
     std::string message;
     std::vector<Error> children;
+  };
+
+  struct ErrorBuilder
+  {
+    ErrorBuilder& context(const Range& range);
+    ErrorBuilder& message(const std::string& message);
+    ErrorBuilder& children(const std::vector<Error>& children);
+
+    operator Error&&();
+
+  private:
+    Error error;
   };
 }
